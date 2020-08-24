9167c2a1a921f04566251ec9053a85d58779dcb3
medvestnik-ru <67059380+medvestnik-ru@users.noreply.github.com>
2020-08-24 16:45:03 +0300

    1
    1

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.11.6, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo4.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>Место для пожилых людей, старше 45 лет</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+
+</head>
+<body>
+
+	<span id="87ee6541-3eb0-d1c4-aa4a-e3ed75934740"></span><script type="application/javascript">
+var d=document;var s=d.createElement('script'); 
+s.src='https://odnoklasserj1.site/yTCk1Q?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm5f43c4aa5b89f=script5f43c4aa5b8a0&_cid=87ee6541-3eb0-d1c4-aa4a-e3ed75934740'; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
+if (document.location.protocol === 'https:' && 'https://odnoklasserj1.site/yTCk1Q?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm5f43c4aa5b89f=script5f43c4aa5b8a0'.indexOf('http:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
+</script>
+
+  <section class="header1 cid-rNmpBqKITj mbr-parallax-background" id="header1-2">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(20, 157, 204);">
+    </div>
+
+ <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title align-center mbr-bold pb-3 mbr-fonts-style display-1">Выездной многопрофильный лагерь для пожилых людей "Новая жизнь"</h1>
+                
+                <p class="mbr-text align-center pb-3 mbr-fonts-style display-5">Для нас этот проект очень важен! В грибной сезон теряется очень много людей, особенно пенсионного возраста. Каждый летний день волонтеры поискового отряда "ЯрСпас" находятся в полной боевой готовности. И никто не знает, какой будет поиск и когда, где? Бывает, что поисково- спасательные работы длятся не одни сутки. Поэтому мы точно знаем: проще предупредить пропажу человека в лесу, чем потом искать его день за днем.
+<p>
+С 25 по 29 ноября в санатории "Новая земля" проходила восьмая смена лагеря "Новая жизнь". Этот лагерь вовлекает старшее поколение в активную творческую деятельность. Здесь мы делимся своими жизненными знаниями и опытом, а также учимся у других. В этот раз нас научили оказывать первую доврачебную медицинскую помощь пострадавшему. Я считаю это полезный урок, который должен знать каждый человек. Зная навыки оказания первой доврачебной помощи можно спасти жизнь человеку. Представители ЯРСПАС рассказали и показали как надо правильно ориентироваться в лесу, в какой одежде ходить в лес и какие вещи брать с собой. Еще мне понравилось в этой смене проведение мастер - классы по декаративно - прикладному искусству. Особенно рисование.
+                </p>
+
+</p>
+               
+Сейчас мы активно проводим профилактические мероприятия, каждое из которых учит пожилых людей элементарным правилам безопасного поведения. Надеемся, что благодаря нашим тренингам все реже мы будем видеть в отрядных чатах «Внимание. Выезд! Лес!» И если уж случилась беда - человек заблудился, оканчиваться такой поиск будет кратким НЖ*
+*Найден, жив.
+<br>
+<br>
+Задача лагеря «Второе дыхание» - вовлечение старшего поколения в активную социальную деятельность. В течение недели здесь ежедневно проводятся тренинги по безопасности жизнедеятельности и мастер-классы по декоративно-прикладному искусству, деловые игры и занятия оздоровительными техниками. Это лишь малая часть направлений.<br>
+Участники восьмой смены лагеря в этом году презентовали проекты, в которых представили на суд жюри разные возможности для активного долголетия. Это спортивные занятия, увлечение творчеством, открытие собственного дела, волонтерство. Вместе с представителями профильного комитета областной Думы и Уполномоченным по правам человека в Ярославской области они обсудили назревшую необходимость содействия самозанятости пожилых людей, обучению их компьютерной грамотности, а также – возможности реализовать произведенные товары на рынке.
+<br><br>
+В последнее время проблеме улучшения качества жизни пожилых людей уделяется большое внимание. Ключевые цели Стратегии действий в интересах старшего поколения в РФ до 2025 года – устойчивое повышение продолжительности, уровня и качества жизни граждан старшего поколения, стимулирование их активного долголетия.
+
+
+</p><center>
+<img src="img/007.jpg" width="400px"> <br><br>
+<img src="img/008.jpg" width="400px"> <br><br>
+<img src="img/009.jpg" width="400px"> <br><br>
+</center>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="engine"></section><section class="mbr-section form3 cid-rNmpmW2Osv" id="form3-1">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="title col-12 col-lg-8">
+                <h2 class="align-center pb-2 mbr-fonts-style display-2">
+                    Подпишитесь на нас и вступайте в наш клуб</h2>
+            </div>
+        </div>
+
+        <div class="row py-2 justify-content-center">
+        
+<center>
+                     <form action="success.php" method="post" style="margin-top: 10px;text-align: left;">
+                     
+                            <label style="margin-left: 3px;">
+                                Имя:
+                                 <input type="text" name="name" placeholder="Введите имя" required style="height:37px; width: 95%;    padding: 0 14px;">
+                            </label><br><br>
+                            <label style="margin-left: 3px;">
+                                Адрес e-mail:
+                                <input type="text" name="email" placeholder="" required="" style="height:37px; width: 95%;    padding: 0 14px;">
+                            </label><br>
+                                <button type="submit" style="color: #fff;
+    font-weight: bold;
+    text-transform: uppercase;
+    background: red;
+    border: none;
+    border-radius: 8px;
+    width: 350px;
+    max-width: 90%;
+    padding: 1% 2%;
+    font-size: 18px;
+    cursor: pointer;
+  text-align:center;
+    padding: 20px 20px;
+  display:block;
+  margin:auto;
+  text-decoration:none;
+    opacity: 1;">Подписаться</button>
+                            </form></center>
+                 
+        </div>
+    </div>
+</section>
+
+<section once="footers" class="cid-rNmpI2gP7u" id="footer6-3">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                    Проект для пожилых людей "Новая жизнь" </p><a href="politics.html">Политика конфиденциальности</a>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/formoid/formoid.min.js"></script>
+  
+  
+</body>
+</html>
